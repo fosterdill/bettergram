372446926ac74009616c42f5801f819a2070409e
Dylan Foster <fosterdill@gmail.com>
2014-01-14 19:33:55 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bettergram::Application.config.secret_token = 'd27f2c6c49524eb8de75c5ea32120dd55dab64b87dc056583844aa46718a358eadd074bab4aa601692ec94c626fb9e46ca2bdad6fd3f67de53d53d5c922f3543'
