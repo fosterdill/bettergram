bcfe4c26834b19ff22215a3be18a67df3743f02f
Dylan Foster <fosterdill@gmail.com>
2014-01-16 18:51:57 -0800

    fix styles

--- app/assets/javascripts/views/photos_index.js
+++ app/assets/javascripts/views/photos_index.js
@@ -3,18 +3,25 @@ Bettergram.Views.PhotosIndex = Backbone.View.extend({
   render: function () {
     var that = this;
     var rows = new Bettergram.Collections.Photos();
+    var lastId;
     this.collection.each(function (item, iterator) {
       rows.add(item);
       if ((iterator + 1) % 3 === 0) {
         var photoRow = new Bettergram.Views.PhotoRow({ collection: rows });
         that.$el.append(photoRow.render().$el);
+        that.lastId = rows.last().get('id');
         rows = new Bettergram.Collections.Photos();
       }
       if (that.collection.length - 1 === iterator) {
         var photoRow = new Bettergram.Views.PhotoRow({ collection: rows });
         that.$el.append(photoRow.render().$el);
+        that.lastIndex = iterator;
       }
     });
+    this.lastId = this.lastId || rows.last().get('id');
     return this;
+  },
+
+  addPhotos: function () {
   }
 });
