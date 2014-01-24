8d167c376d81eb347c0dad8cb7b61d2998803b34
Dylan Foster <fosterdill@gmail.com>
2014-01-24 10:53:42 -0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140123191616) do
+ActiveRecord::Schema.define(:version => 20140123214053) do
 
   create_table "comments", :force => true do |t|
     t.string   "user_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20140123191616) do
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "username"
   end
 
   create_table "users", :force => true do |t|
