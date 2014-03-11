f9dea6b3ca44734d23b76eb14578981214b76bc9
Dylan Foster <fosterdill@gmail.com>
2014-03-11 02:21:17 -0700

    small refactoring

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,18 +3,19 @@ class Api::UsersController < ApplicationController
       begin
         if params[:id]
           @user = current_instagram_client.user(params[:id])
-          @user[:posts] = current_instagram_client.user_recent_media(params[:id]);
+          @user[:posts] = 
+            current_instagram_client.user_recent_media(params[:id]);
           @user[:following] = 
             current_instagram_client.user_relationship(params[:id])
-          @user[:posts] = get_photo_comments @user[:posts].to_json
         else
           @user = current_instagram_client.user;
           @user[:posts] = current_instagram_client.user_recent_media;
-          @user[:posts] = get_photo_comments @user[:posts].to_json
         end
       rescue Exception => e
         @user = {}
         @user[:errors] = ["User is private"]
+      else
+        @user[:posts] = get_photo_comments @user[:posts].to_json
       end
       render :json => @user
   end
